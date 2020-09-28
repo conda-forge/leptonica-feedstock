3678e1b24c3adae7230fba12cc8db7c3deef30e5
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-09-29 00:54:27 +0200

    Download, extract and run sw for Win build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,12 @@
+curl -fsSOL https://software-network.org/client/sw-master-windows-client.zip
+if errorlevel 1 exit 1
+
+unzip sw-master-windows-client.zip -d .
+if errorlevel 1 exit 1
+
+sw setup
+if errorlevel 1 exit 1
+
 mkdir build
 cd build
 
