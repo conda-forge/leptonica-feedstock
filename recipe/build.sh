8048b23060203910785c94ac530a3e2bf90a6f73
Ista Zahn <izahn@hbs.edu>
2021-05-20 09:00:01 -0400

    bugfix

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 ./autogen.sh
 ./configure --prefix=$PREFIX --disable-dependency-tracking
-sed -i 's/TARGET_OS_OSX/TARGET_OS_MAC/g' src/utils2.c ## https://github.com/DanBloomberg/leptonica/issues/579
+[[ $(uname) = "Darwin" ]] && sed -i '' 's/TARGET_OS_OSX/TARGET_OS_MAC/g' src/utils2.c ## https://github.com/DanBloomberg/leptonica/issues/579
 make -j$CPU_COUNT
 make install
