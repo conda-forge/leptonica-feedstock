f92c29088c5bad8c58f682a83243c49c8ca5e23e
Peter Williams <peter@newton.cx>
2023-07-29 16:12:58 +0000

    recipe/bld.bat: make final step robust against version changes (hopefully)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -29,5 +29,5 @@ cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 :: Make copies of the .lib file without the embedded version number
-copy %LIBRARY_LIB%\leptonica-1.82.0.lib %LIBRARY_LIB%\leptonica.lib
-copy %LIBRARY_LIB%\leptonica-1.82.0.lib %LIBRARY_LIB%\lept.lib
+copy %LIBRARY_LIB%\leptonica-%PKG_VERSION%.lib %LIBRARY_LIB%\leptonica.lib
+copy %LIBRARY_LIB%\leptonica-%PKG_VERSION%.lib %LIBRARY_LIB%\lept.lib
