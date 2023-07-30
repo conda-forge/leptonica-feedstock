22958476356a2b8052a24c4c2e8378fa4ff7b342
Peter Williams <peter@newton.cx>
2023-07-30 16:01:21 +0000

    Don't use `sw` when building on Windows
    After all that effort to get it to work ... it turns out that the build just
    works on Windows if we don't use `sw`, and just pick up the conda-forge
    dependencies. And in my testing, we need a non-`sw` build in order to
    successfully build Tesseract against the newest Leptonica.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,34 +1,16 @@
 mkdir build
 cd build
 
-curl -fsSOL https://software-network.org/client/sw-master-windows_x86_64-client.zip
-if errorlevel 1 exit 1
-
-unzip sw-master-windows_x86_64-client.zip -d .
-if errorlevel 1 exit 1
-
-set PATH=%PATH%;%CD%
-
-sw setup
-if errorlevel 1 exit 1
-
-:: As of July 2023, the build system for this package can run into issues if the
-:: version of `cl` found via %PATH% is not exactly the same as the version used
-:: by MSBuild. The only way I can figure out to avoid the problem is to avoid
-:: MSBuild altogether, which can be done by using the NMake Makefiles generator,
-:: as well as explicitly specifying the full path to the desired compilers.
-set "clpathunix=%VCToolsInstallDir%bin\Hostx64\x64\cl.exe"
-set "clpathunix=%clpathunix:\=/%"
-
 cmake %CMAKE_ARGS% ^
       -D BUILD_PROG=1 ^
+      -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D SW_BUILD=OFF ^
       -D BUILD_SHARED_LIBS=ON ^
       -D CMAKE_MODULE_LINKER_FLAGS=-whole-archive ^
-      -D "CMAKE_C_COMPILER=%clpathunix%" ^
-      -D "CMAKE_CXX_COMPILER=%clpathunix%" ^
       -G "NMake Makefiles" ^
       ..
 if errorlevel 1 exit 1
